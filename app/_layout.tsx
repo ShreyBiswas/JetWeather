6740278d3e0083348d7a14b9f083fc98da2a4c4d
Shrey Biswas <shrey.biswas@gmail.com>
2024-05-16 14:49:49 +0100

    updating with navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,64 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
+import React from "react";
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+
+import SwitchingButton from "@/components/navigation/SwitchingButton";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+    const colorScheme = useColorScheme();
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="(tabs)/cities"
-        options={{
-          title: 'Saved Cities',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'location' : 'location-outline'} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
-  );
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+                headerShown: false,
+            }}
+        >
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: "Home",
+                    tabBarIcon: ({ color, focused }) => (
+                        <TabBarIcon
+                            name={focused ? "home" : "home-outline"}
+                            color={color}
+                        />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="(tabs)/cities"
+                options={{
+                    title: "Saved Cities",
+                    tabBarIcon: ({ color, focused }) => (
+                        <TabBarIcon
+                            name={focused ? "location" : "location-outline"}
+                            color={color}
+                        />
+                    ),
+                    tabBarButton: () => (
+                        <SwitchingButton
+                            iconName="grid"
+                            destinationScreen="(tabs)/cities"
+                        />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="(tabs)/NeumorphicTests"
+                options={{
+                    title: "Neumorphic Tests",
+                    tabBarIcon: ({ color, focused }) => (
+                        <TabBarIcon
+                            name={focused ? "home" : "home-outline"}
+                            color={color}
+                        />
+                    ),
+                }}
+            />
+        </Tabs>
+    );
 }
